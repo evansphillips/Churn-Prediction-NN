26f2f278b3f63c0887a7bf72ff96349c3c2a763b
evansphillips <evansphillips@berkeley.edu>
2023-09-01 22:48:51 -0700

    newly PEP-8 formatted Python sections

--- notebooks/metrics.py
+++ notebooks/metrics.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 def f1_score(y_true, y_pred):
     """
     Calculate F1 score using TensorFlow functions.
@@ -13,7 +14,10 @@ def f1_score(y_true, y_pred):
     """
     precision = precision_m(y_true, y_pred)
     recall = recall_m(y_true, y_pred)
-    return 2 * ((precision * recall) / (precision + recall + tf.keras.backend.epsilon()))
+    return 2 * (
+        (precision * recall) / (precision + recall + tf.keras.backend.epsilon())
+    )
+
 
 def precision_m(y_true, y_pred):
     """
@@ -26,10 +30,15 @@ def precision_m(y_true, y_pred):
     Returns:
         tf.Tensor: Precision.
     """
-    true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_pred, 0, 1)))
+    true_positives = tf.keras.backend.sum(
+        tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1))
+    )
+    predicted_positives = tf.keras.backend.sum(
+        tf.keras.backend.round(tf.keras.backend.clip(y_pred, 0, 1))
+    )
     precision = true_positives / (predicted_positives + tf.keras.backend.epsilon())
     return precision
+
 
 def recall_m(y_true, y_pred):
     """
@@ -42,7 +51,11 @@ def recall_m(y_true, y_pred):
     Returns:
         tf.Tensor: Recall.
     """
-    true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
-    actual_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true, 0, 1)))
+    true_positives = tf.keras.backend.sum(
+        tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1))
+    )
+    actual_positives = tf.keras.backend.sum(
+        tf.keras.backend.round(tf.keras.backend.clip(y_true, 0, 1))
+    )
     recall = true_positives / (actual_positives + tf.keras.backend.epsilon())
     return recall
